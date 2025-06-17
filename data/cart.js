9d0645e8c66fde51d368b45b8384676ea928e682
Swadeep <dhondiswadeepdsp6@gmail.com>
2025-06-17 06:46:19 +0530

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,23 @@
+
 export const cart = [];
+export function addToCart(productId) {
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === item.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  const quantitySelector = document.querySelector(
+    `.js-quantity-selector-${productId}`
+  );
+  let quantity = quantitySelector.value;
+  quantity = Number(quantitySelector.value);
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({
+      productId,
+      quantity,
+    });
+  }
+}
